50ad1e8ae1df18bc5662b1e0033375bcdda64d79
SMSHKR <39004788+SMSHKR@users.noreply.github.com>
2018-10-11 00:15:44 +0700

    Add input() for numberOfTree

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int numberOfTree = 0;
+        while (numberOfTree == 0) numberOfTree = inputTree();
+
     }
+
+    private static int inputTree() {
+        // FIXME NoSuchElementException
+        int numberOfTree;
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("#Trees : ");
+            numberOfTree = scan.nextInt();
+            if (numberOfTree < 0) throw new Exception();
+        } catch (Exception e) {
+            // e.printStackTrace();
+            numberOfTree = 0;
+            System.err.println("Invalid Input, Please Try Again.");
+        }
+
+        return numberOfTree;
+
+    }
+
 }
